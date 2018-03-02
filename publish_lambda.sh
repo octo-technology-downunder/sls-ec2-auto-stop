021026362b046265b9086e1944850d06fbca0406
Ilya Trofimov <itrofimov@octo.com>
2018-03-02 18:47:14 +1100

    Trying to fix deployment issue#5

--- publish_lambda.sh
+++ publish_lambda.sh
@@ -25,7 +25,7 @@ if [[ ${lambda_exists} ]]; then
     aws serverlessrepo create-application-version \
     --application-id ${lambda_id} \
     --semantic-version ${version} \
-    --source-code-url \'${source_code_url}\' \
+    --source-code-url "`${source_code_url}`" \
     --template-body ${new_template_file} \
     --region ap-southeast-2
 else
