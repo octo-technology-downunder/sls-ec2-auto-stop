6b484396433026fc77a8cd490417f481245231ec
Baudry Arthur <abaudry@octo.com>
2018-03-02 12:39:44 +1100

    Deployment script for circle CI

---
+++ publish_lambda.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -ex
+
+lambda_name="ec2-autostop"
+new_template_file="template_s3.yml"
+
+#Packaging and sending package to S3, updating template with S3 URI
+aws cloudformation package --template-file template.yml --s3-bucket serverless-public --s3-prefix $lambda_name --output-template-file $new_template_file
+
+# Verify if lambda already published
+lambda_name_exists=$(aws serverlessrepo list-applications --query "Applications[?Name==\`$lambda_name\`].Name" --output text)
+lambda_exists=true
+if [[ -z "$lambda_name_exists" ]]; then
+    echo "Lambda not found"
+    lambda_exists=false
+fi
+
+version='0.1.1'
+source_code_url='https://github.com/octo-technology-downunder/sls-ec2-auto-stop.git'
+
+if [[ $lambda_exists ]]; then
+    echo "Lambda found"
+    lambda_id=$(aws serverlessrepo list-applications --query "Applications[?Name==\`$lambda_name\`].ApplicationId" --output text)
+    echo "Application id is $lambda_id"
+    aws serverlessrepo create-application-version \
+    --application-id $lambda_id \
+    --semantic-version $version \
+    --source-code-url $source_code_url \
+    --template-body $new_template_file
+else
+    aws serverlessrepo create-application \
+    --author "Octo Technology" \
+    --description "Stopping EC2 instances automatically" \
+    --home-page-url https://www.octo.com/en \
+    --labels EC2 \
+    --name $lambda_name \
+    --semantic-version $version \
+    --source-code-url $source_code_url \
+    --spdx-license-id Apache-2.0 \
+    --template-body $new_template_file
+fi
